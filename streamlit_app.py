9950bbe9fe5e737bebebe778658564499d1ecc04
AA3141 <amritz.fa@gmail.com>
2021-03-29 19:06:22 +0100

    Increase photo size(height and width)

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,8 +78,8 @@ def main():
           'toImageButtonOptions': {
             'format': 'svg', # one of png, svg, jpeg, webp
             'filename': 'custom_image',
-            'height': 500,
-            'width': 700,
+            'height': 1000,
+            'width': 1400,
             'scale': 1 # Multiply title/legend/axis/canvas sizes by this factor
           }
         }
